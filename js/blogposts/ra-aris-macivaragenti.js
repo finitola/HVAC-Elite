d139a288f438abc56e43b51f52dc37663cf1940f
George <41702152+finitola@users.noreply.github.com>
2026-05-31 22:24:29 +0400

    Update image URL for blog post 'ra-aris-macivaragenti'

--- js/blogposts/ra-aris-macivaragenti.js
+++ js/blogposts/ra-aris-macivaragenti.js
@@ -1,7 +1,7 @@
 export const meta = {
 	id: '1',
 	slug: 'ra-aris-macivaragenti',
-	image: '../../images/blogposts/ra-aris-macivaragenti-freoni.webp',
+	image: 'https://hvacelite.ge/images/blogposts/ra-aris-macivaragenti-freoni.webp',
 	ka: {
 		title: 'რა არის მაცივარაგენტი (ფრეონი) ?',
 		subtitle: 'მაცივარაგენტი (ფრეონი) - უხილავი გმირი კონდიცირების სისტემაში',
