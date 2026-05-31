32d9b7fe31bd114b7a1933b6c77d9af4af3c912b
George <41702152+finitola@users.noreply.github.com>
2026-05-31 22:24:10 +0400

    Update blog post content and image URL

--- js/blogposts/inverteruli-tegnologia-vs-on-off.js
+++ js/blogposts/inverteruli-tegnologia-vs-on-off.js
@@ -2,7 +2,7 @@ export const meta = {
 	id: '3',
 	slug: 'inverteruli-teqnologia-vs-on-off',
 	image:
-		'../../images/blogposts/invertoruli-vs-on-off-kondicirebis-sistema.webp',
+		'https://hvacelite.ge/images/blogposts/invertoruli-vs-on-off-kondicirebis-sistema.webp',
 	ka: {
 		title: 'AC Inverter VS AC ON/OFF',
 		subtitle:
