987a2a356ef64e15dfd8b785dfd6ad5cbd2d7d38
George <41702152+finitola@users.noreply.github.com>
2026-06-01 22:49:07 +0400

    Add Telegram button to Contact page

--- js/pages/Contact.js
+++ js/pages/Contact.js
@@ -90,6 +90,27 @@ export default function Contact(t) {
 				</svg>
 				WhatsApp
 			</a>
+			<a
+				class="contact-btn contact-btn-tg"
+				href="https://t.me/+995514128821"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<svg
+					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"
+					fill="currentColor"
+					width="24"
+					height="24"
+					style="flex-shrink: 0"
+				>
+					<path
+						d="M21.944 4.517a1 1 0 0 0-1.104-.163L2.84 11.66a1 1 0 0 0 .104 1.865l4.79 1.597 1.598 4.79a1 1 0 0 0 .796.672 1 1 0 0 0 .953-.41L22.108 5.62a1 1 0 0 0-.164-1.103zM9.39 14.61l-.96 3.2-.99-2.97 8.46-6.68-6.51 6.45z"
+					/>
+				</svg>
+				Telegram
+			</a>
 		</div>
 	</div>
 	<div class="contact-column">
