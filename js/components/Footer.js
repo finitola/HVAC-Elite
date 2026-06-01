5f66ed0e8a2c9c607860c183760413154f9250cd
George <41702152+finitola@users.noreply.github.com>
2026-06-02 01:41:50 +0400

    Update Footer component with counter script
    Added async counter script for tracking and updated footer structure.

--- js/components/Footer.js
+++ js/components/Footer.js
@@ -197,7 +197,13 @@ export default function Footer(t, currentLang) {
 		</ul>
 	</div>
 
-	<div class="footer-bottom"><a href="/">&copy; ${currentYear} ${t.footer_text}</a></div>
+	<div class="footer-bottom">
+		<a href="/">&copy; ${currentYear} ${t.footer_text}</a>
+	    <!-- TOP.GE ASYNC COUNTER CODE -->
+        <div id="top-ge-counter-container" data-site-id="118773"></div>
+        <script async src="//counter.top.ge/counter.js"></script>
+        <!-- / END OF TOP.GE COUNTER CODE -->	
+	</div>
 </footer>
 <!-- Float Button Up -->
 <button id="scrollToTopBtn" class="scroll-to-top" aria-label="Scroll to top">
